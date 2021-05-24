25ba1511cf3dd512364bbe6a77dc9f637862332a
csoyn <csy51113@naver.com>
2021-05-24 20:35:45 +0900

    랭크 별점 추가

--- movies/forms.py
+++ movies/forms.py
@@ -3,20 +3,46 @@ from django.forms import widgets
 from .models import Movie, Review, Comment
 
 class ReviewForm(forms.ModelForm):
+    CHOICES= [
+        (1, '★☆☆☆☆'),
+        (2, '★★☆☆☆'),
+        (3, '★★★☆☆'),
+        (4, '★★★★☆'),
+        (5, '★★★★★')
+    ]
+    rank = forms.IntegerField(
+        label='별점',
+        widget=forms.Select(
+            choices = CHOICES,
+            attrs={
+                'style': 'width: 10rem;'
+            }
+        )
+    )
+    title = forms.CharField(
+        label='글 제목',
+        widget=forms.TextInput(
+            attrs={'style': 'height: 2rem;' , 'class': 'form-control'}
+        )
+    )
+    content = forms.CharField(
+        label='글 내용',
+        widget=forms.Textarea(
+            attrs={'style': 'height: 10rem;', 'class': 'form-control'}
+        )
+    )
     class Meta:
         model = Review
-        fields = ('title', 'rank', 'content',)
-        widgets = {
-            'title': forms.TextInput(attrs= {'class': 'form-control'}),
-            'rank': forms.Select(attrs= {'class': 'form-control'}),
-            'content': forms.Textarea(attrs= {'class': 'form-control'}),
-        }
+        fields = ['title', 'rank', 'content',]
 
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'style': 'height: 2rem;', 'class': 'form-control'}
+        )
+    )
     class Meta:
         model = Comment
         fields = ('content', )
-        widgets = {
-            'content': forms.Textarea(attrs= {'class': 'form-control'}),
-        }
+    
